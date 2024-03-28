5cbef4563535e08a203151101a72c351cfcdf22e
Shadrak Lagadi <shadrl19@gmail.com>
2024-03-28 13:59:52 -0600

    Refactor component to take in props from App.jsx and display requirements

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,8 +3,15 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  return (
+    <>
+      <img src={props.data.imageSource} alt="Photo" />
+      <img src={props.data.profile} alt="Profile" />
+      <div>{props.data.username}</div>
+      <div>{props.data.location.city} {props.data.location.country}</div>
+    </>
+  )
 };
 
 export default PhotoListItem;
