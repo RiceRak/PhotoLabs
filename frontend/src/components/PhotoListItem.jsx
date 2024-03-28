5814d65d62f158fa57cf59eebdc93502eb51da95
Shadrak Lagadi <shadrl19@gmail.com>
2024-03-28 14:09:06 -0600

    Fix fragment to div. Add more description to alt text as per Compass AI

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,13 +4,14 @@ import "../styles/PhotoListItem.scss";
 
 
 const PhotoListItem = (props) => {
+
   return (
-    <>
-      <img src={props.data.imageSource} alt="Photo" />
+    <div>
+      <img src={props.data.imageSource} alt="Photo taken by {props.data.username}" />
       <img src={props.data.profile} alt="Profile" />
       <div>{props.data.username}</div>
       <div>{props.data.location.city} {props.data.location.country}</div>
-    </>
+    </div>
   )
 };
 
