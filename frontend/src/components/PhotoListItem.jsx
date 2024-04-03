6fe892b7651201971fb83d40f04955429ea77f83
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-03 15:59:31 -0600

    Add toggleFavourite function. Import useState. Import PhotoFavButton. Pass state to PhotoFavButton

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
 
 const PhotoListItem = (props) => {
+  
+  const [isFavourite, setIsFavourite] = useState(false);
+
+ // Define the onClick event handler function
+ const toggleFavourite = () => {
+  // Implement desired functionality here
+  setIsFavourite(!isFavourite);
+};
 
   return (
     <article className="photo-list__item">
-      <PhotoFavButton />
+      
+      <PhotoFavButton selected={isFavourite} onClick={toggleFavourite} />
+
       <img className="photo-list__image" src={props.data.imageSource} alt="Photo taken by {props.data.username}" />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={props.data.profile} alt="Profile" />
