2c3277371c56c5309d6339443e4bcac08c5f3cc0
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-03 15:19:32 -0600

    Add handleClick function to console.log test message when clicked

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,8 +4,14 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+ // Define the onClick event handler function
+ const handleClick = () => {
+  // Implement desired functionality here
+  console.log('Favorite button clicked!');
+};
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
         {/* Insert React */}
       </div>
