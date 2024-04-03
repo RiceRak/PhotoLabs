a8604a7748d8eab7db36b1a3db10dcde60d9d281
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-03 15:05:41 -0600

    Fix <div> to show excatly what Compass shows. Rename parent tag to <article>

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,14 +6,16 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
 
   return (
-    <div className="photo-list__item">
+    <article className="photo-list__item">
       <img className="photo-list__image" src={props.data.imageSource} alt="Photo taken by {props.data.username}" />
       <div className="photo-list__user-details">
-      <img className="photo-list__user-profile" src={props.data.profile} alt="Profile" />
-      <div className="photo-list__user-info">{props.data.username}</div>
-      <div className="photo-list__user-location">{props.data.location.city} {props.data.location.country}</div>
+        <img className="photo-list__user-profile" src={props.data.profile} alt="Profile" />
+        <div className="photo-list__user-info">{props.data.username}
+          <div className="photo-list__user-location">{props.data.location.city}, {props.data.location.country}
+          </div>
+        </div>
       </div>
-    </div>
+    </article>
   )
 };
 
