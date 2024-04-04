7d3ad5dcde6b4737a91a76059b2f35dab35704bb
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-04 14:57:20 -0600

    Import TopicList component and render in react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
+import TopicList from 'components/TopicList';
 
 const App = () => {
 
@@ -18,6 +19,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <TopicList />
       {/* {renderedPhotos} */}
       <PhotoList />
     </div>
