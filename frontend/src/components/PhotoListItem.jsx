331d5c80a9c6ab45b1ff4455218069503a7a0f7a
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-03 22:35:42 -0600

    Fix component to take in new props sent from PhotoList component. Destructor the props and correct dot notation to display correct content

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,27 +4,24 @@ import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
 
-const PhotoListItem = (props) => {
-
+const PhotoListItem = ({ data }) => {
+  const { user, urls, location } = data;
   const [isFavourite, setIsFavourite] = useState(false);
 
- // Define the onClick event handler function
- const toggleFavourite = () => {
-  // Implement desired functionality here
-  setIsFavourite(!isFavourite);
-};
+  // Define the onClick event handler function
+  const toggleFavourite = () => {
+    setIsFavourite(!isFavourite);
+  };
 
   return (
-    <article className="photo-list__item">
-      
+    <article className="photo-list__item">      
       <PhotoFavButton selected={isFavourite} onClick={toggleFavourite} />
-
-      <img className="photo-list__image" src={props.data.imageSource} alt={`Photo taken by ${props.data.username}`} />
+      <img className="photo-list__image" src={urls.regular} alt={`Photo taken by ${user.username}`} />
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={props.data.profile} alt="Profile" />
-        <div className="photo-list__user-info">{props.data.username}
-          <div className="photo-list__user-location">{props.data.location.city}, {props.data.location.country}
-          </div>
+        <img className="photo-list__user-profile" src={user.profile} alt="Profile" />
+        <div className="photo-list__user-info">
+          {user.username}
+          <div className="photo-list__user-location">{location.city}, {location.country}</div>
         </div>
       </div>
     </article>
