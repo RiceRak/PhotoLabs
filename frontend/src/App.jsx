0118662a9e09d71f6066baca7eaef5873e1e7ef6
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-04 17:23:17 -0600

    Clean up unused code and comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,20 +8,9 @@ import TopNavigation from 'components/TopNavigationBar';
 
 const App = () => {
 
-// COMMENT OUT AS PER COMPASS
-  // create an array with 3 values
-  // const photos = [1, 2, 3];
-// Utilize the .map method to iterate through each element of the 'photos' array,
-// naming each element as 'photo'. For each iteration, invoke a callback function
-// to create a 'PhotoListItem' component, with a unique key assigned to each photo.
-// 'sampleDataForPhotoListItem' is to be a constant or variable containing
-// relevant data for rendering the 'PhotoListItem' component.
-  // const renderedPhotos = photos.map((photo) => <PhotoListItem key={photo} data={sampleDataForPhotoListItem} />)
-
   return (
     <div className="App">
       <TopNavigation />
-      {/* {renderedPhotos} */}
       <PhotoList />
     </div>
   );
