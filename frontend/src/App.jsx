a63801a55a93609e6c4c07eb89b6cfa47eba9143
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-04 18:22:56 -0600

    Import photos. Pass down photos as a prop to <HomeRoute />

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import topics from './mocks/topics'
+import photos from './mocks/photos'
 
 const App = () => {
 
   return (
     <div className="App">
-      <HomeRoute topics={topics}/>
+      <HomeRoute topics={topics} photos={photos}/>
     </div>
   );
 };
