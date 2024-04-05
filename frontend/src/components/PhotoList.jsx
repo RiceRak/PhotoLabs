99e7819fda6744895526e7e78d69e08324ac93dd
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-04 18:25:16 -0600

    Implement props passed down from <HomeRoute /> and fix map method to use new props

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
-import photos from "../mocks/photos"
 
-const PhotoList = () => {
+const PhotoList = (props) => {
   return (
 <ul className="photo-list">
-  {photos.map(item => (
+  {props.photos.map(item => (
     <PhotoListItem key={item.id} data={item} />
   ))}
 </ul>
