fc82aedb30b59a4c124cea50560e66d42decbc58
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-06 14:11:44 -0600

    Import <PhotoDetailsModal />. Create 'showModal' state and set it to false. Define 'clickOnPhoto' function that will change state to true. Pass function down to <HomeRoute />. Add conditional render of <PhotoDetailsModal />

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import topics from './mocks/topics'
 import photos from './mocks/photos'
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 const App = () => {
 
+  const [showModal, setShowModal] = useState(false);
+
+  const clickOnPhoto = () => {
+    setShowModal(true);
+  };
+
   return (
     <div className="App">
-      <HomeRoute topics={topics} photos={photos}/>
+      <HomeRoute topics={topics} photos={photos} clickOnPhoto={clickOnPhoto}/>
+      {showModal && <PhotoDetailsModal />}
     </div>
   );
 };
