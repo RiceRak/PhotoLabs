54137a0594b7620952a81de526a7be1474d8fce6
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-06 14:49:17 -0600

    Deconstruct props passed from App.jsx. Define 'CloseModal' function to use 'setShowModal' update function. Assign onClick handler to 'photo-details-modal__close-button' to call 'closeModal' function

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = (props) => {
+  const { showModal, setShowModal } = props
+
+  const closeModal = () => {
+    setShowModal(false);
+  }
+
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
+        <img src={closeSymbol} alt="close symbol" onClick={closeModal}/>
       </button>
     </div>
   )
