239f2557bb0da6f927fd66acc5a6350329d67d76
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-06 11:38:39 -0600

    Bring state and 'toggleFavourite' to <HomeRoute />. Refactor 'toggleFavourite'. Create faveStateControl and pass down as props to <PhotoList />

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 
 import '../styles/HomeRoute.scss';
 import PhotoList from 'components/PhotoList';
 import TopNavigation from 'components/TopNavigationBar';
 
-
 const HomeRoute = (props) => {
+
+  const [isFavourite, setIsFavourite] = useState([]);
+
+  const toggleFavourite = (photoId) => {
+    setIsFavourite(prev => {
+      if (!prev.includes(photoId)) {
+        return [
+          ...prev,
+          photoId
+        ]
+      }
+      return prev.filter(element => element !== photoId)
+    });
+  };
+
+  const favStateControl = {
+    isFavourite,
+    toggleFavourite,
+  };
+
   return (
     <div className="home-route">
-      <TopNavigation topics={props.topics}/>
-      <PhotoList photos={props.photos}/>
+      <TopNavigation topics={props.topics} />
+      <PhotoList photos={props.photos} favStateControl={favStateControl} />
     </div>
   );
 };
