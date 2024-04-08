80e5d9fe5d068746642964e515cccae61629b406
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-08 14:05:55 -0600

    Remove logic that manages the state into new hook 'useApplicationData'. Import new hook and deconstruct useApplicationData

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,40 +5,33 @@ import HomeRoute from 'routes/HomeRoute';
 import topics from './mocks/topics'
 import photos from './mocks/photos'
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
-import PhotoList from 'components/PhotoList';
+import useApplicationData from './hooks/useApplicationData';
 
 const App = () => {
-  const [isFavourite, setIsFavourite] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
-
-  const toggleFavourite = (photoId) => {
-    setIsFavourite(prev => {
-      if (!prev.includes(photoId)) {
-        return [
-          ...prev,
-          photoId
-        ]
-      }
-      return prev.filter(element => element !== photoId)
-    });
-  };
-
-  const clickOnPhoto = (photo) => {
-    setSelectedPhoto(photo);
-    setShowModal(true);
-  };
-
-  const favStateControl = {
+  const {
     isFavourite,
+    showModal,
+    setShowModal,
+    selectedPhoto,
     toggleFavourite,
-  };
+    clickOnPhoto,
+    favStateControl,
+  } = useApplicationData();
 
   return (
     <div className="App">
-      <HomeRoute topics={topics} photos={photos} clickOnPhoto={clickOnPhoto} favStateControl={favStateControl} />
-      {showModal && <PhotoDetailsModal setShowModal={setShowModal} selectedPhoto={selectedPhoto} favStateControl={favStateControl} clickOnPhoto={clickOnPhoto} />}
-      
+      <HomeRoute 
+        topics={topics} 
+        photos={photos} 
+        clickOnPhoto={clickOnPhoto} 
+        favStateControl={favStateControl} 
+      />
+      {showModal && 
+        <PhotoDetailsModal 
+          setShowModal={setShowModal} 
+          selectedPhoto={selectedPhoto} 
+          favStateControl={favStateControl} 
+          clickOnPhoto={clickOnPhoto} />}
     </div>
   );
 };
