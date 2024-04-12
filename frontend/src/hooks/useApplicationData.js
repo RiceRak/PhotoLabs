8ee1a35790ebfbf68ba006d2e53a93d7e2de888f
Shadrak Lagadi <shadrl19@gmail.com>
2024-04-12 11:26:04 -0600

    Remove unused code and comments. Add semicolons

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -8,14 +8,14 @@ const initialState = {
   photoData: [],
   topicData: [],
   clickOnTopic: null,
-}
+};
 
-const SET_SELECTED_PHOTO = "SET_SELECTED_PHOTO"
-const SET_PHOTOS = "SET_PHOTOS"
-const SET_TOPICS = "SET_TOPICS"
-const TOGGLE_FAVOURITE = "TOGGLE_FAVOURITES"
-const HIDE_MODAL = "HIDE_MODAL"
-const GET_PHOTOS_BY_TOPIC = "GET_PHOTOS_BY_TOPIC"
+const SET_SELECTED_PHOTO = "SET_SELECTED_PHOTO";
+const SET_PHOTOS = "SET_PHOTOS";
+const SET_TOPICS = "SET_TOPICS";
+const TOGGLE_FAVOURITE = "TOGGLE_FAVOURITES";
+const HIDE_MODAL = "HIDE_MODAL";
+const GET_PHOTOS_BY_TOPIC = "GET_PHOTOS_BY_TOPIC";
 
 
 
@@ -23,25 +23,25 @@ const reducer = (state, action) => {
 
   switch (action.type) {
     case GET_PHOTOS_BY_TOPIC:
-      return { ...state, clickOnTopic: action.payload }
+      return { ...state, clickOnTopic: action.payload };
 
     case SET_PHOTOS:
-      return { ...state, photoData: action.payload }
+      return { ...state, photoData: action.payload };
 
     case SET_TOPICS:
-      return { ...state, topicData: action.payload }
+      return { ...state, topicData: action.payload };
 
     case SET_SELECTED_PHOTO:
-      return { ...state, selectedPhoto: action.payload, showModal: true }
+      return { ...state, selectedPhoto: action.payload, showModal: true };
 
     case TOGGLE_FAVOURITE:
       if (state.favourites.includes(action.payload)) {
-        return { ...state, favourites: state.favourites.filter(element => element !== action.payload) }
+        return { ...state, favourites: state.favourites.filter(element => element !== action.payload) };
       }
-      return { ...state, favourites: [...state.favourites, action.payload] }
+      return { ...state, favourites: [...state.favourites, action.payload] };
 
     case HIDE_MODAL:
-      return { ...state, showModal: false }
+      return { ...state, showModal: false };
 
     default:
       throw new Error(`Unsupported action type: ${action.type}`);
@@ -49,52 +49,38 @@ const reducer = (state, action) => {
 }
 
 const useApplicationData = () => {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const setTopicFilter = (topicId) => {
     dispatch({
       type: GET_PHOTOS_BY_TOPIC,
       payload: topicId
-    })
+    });
   }
 
   useEffect(() => {
-    console.log('inside useEffect 1')
 
     const path = state.clickOnTopic ? `api/topics/photos/${state.clickOnTopic}` : 'api/photos';
 
     fetch(path)
-    .then((res) => res.json())
-    // issue happens here!!!
-    .then((data) => {
-      console.log('inside useEffect 1 .then:', data)
-      dispatch({
-        type: SET_PHOTOS,
-        payload: data,
-      })})  
-    }, [state.clickOnTopic])
-  
-  // useEffect(() => {
-  //   console.log('inside useEffect 2')
-  //   fetch('api/photos')
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     console.log('inside useEffect 2 .then', data)
-  //   dispatch({
-  //     type: SET_PHOTOS,
-  //     payload: data,
-  //   })})
-  //   .catch((error) => console.error('Error fetching photos:', error));
-  // }, []);
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({
+          type: SET_PHOTOS,
+          payload: data,
+        })
+      })
+  }, [state.clickOnTopic])
+
 
   useEffect(() => {
     fetch('api/topics')
-    .then((res) => res.json())
-    .then((data) => dispatch({
-      type: SET_TOPICS,
-      payload: data
-    }))
-    .catch((error) => console.error('Error fetching topics:', error));
+      .then((res) => res.json())
+      .then((data) => dispatch({
+        type: SET_TOPICS,
+        payload: data
+      }))
+      .catch((error) => console.error('Error fetching topics:', error));
   }, []);
 
   const setTopics = (topics) => {
@@ -115,20 +101,20 @@ const useApplicationData = () => {
     dispatch({
       type: TOGGLE_FAVOURITE,
       payload: photoId,
-    })
+    });
   };
 
   const clickOnPhoto = (photo) => {
     dispatch({
       type: SET_SELECTED_PHOTO,
       payload: photo
-    })
+    });
   };
 
   const hideModal = () => {
     dispatch({
       type: HIDE_MODAL,
-    })
+    });
   }
 
   return {
